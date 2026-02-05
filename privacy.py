2fcb4381e219dbb11e89a60a91d645bd6a41888b
Manar <Mnar6771399@gmail.com>
2026-02-05 22:32:59 +0300

    update privacy

--- privacy.py
+++ privacy.py
@@ -1,9 +1,14 @@
+# privacy.py
 import cv2
 
 def blur_person(frame, x1, y1, x2, y2):
     roi = frame[y1:y2, x1:x2]
+
     if roi.size == 0:
         return frame
-    blurred = cv2.GaussianBlur(roi, (51, 51), 0)
+
+  
+    blurred = cv2.GaussianBlur(roi, (99, 99), 30)
+
     frame[y1:y2, x1:x2] = blurred
     return frame
